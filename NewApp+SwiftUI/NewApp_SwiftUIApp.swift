c80b28763e77d5a712682ee10f4a7d241509353e
aselka27 <asjumalieva@gmail.com>
2023-02-20 13:32:36 +0600

    Initial Commit

---
+++ NewApp+SwiftUI/NewApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApp_SwiftUIApp.swift
+//  NewApp+SwiftUI
+//
+//  Created by саргашкаева on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
